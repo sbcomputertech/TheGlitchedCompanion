f0b56e42aeb6e33371a3f05e2232d4e693d94329
Callie / reddust9 <78689018+sbcomputertech@users.noreply.github.com>
2023-07-30 12:58:50 +0100

    fix version text patch

---
+++ TGC.Client/PatchVersionText.cs
@@ -0,0 +1,14 @@
+﻿using HarmonyLib;
+
+namespace TGC.Client;
+
+[HarmonyPatch(typeof(MainMenu_VersionTextManager))]
+public static class PatchVersionText
+{
+    [HarmonyPatch(nameof(MainMenu_VersionTextManager.Start))]
+    [HarmonyPostfix]
+    private static void PatchTextWithModNameVersion(ref MainMenu_VersionTextManager __instance)
+    {
+        __instance.txt.text += $"\n{PluginInfo.PLUGIN_NAME} v{PluginInfo.PLUGIN_VERSION}";
+    }
+}
